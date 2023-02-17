0359b0bc5c2ae00a9e0d8ff2d50febf83e5b29b1
Volodymyr Labunets <vplabunets@gmail.com>
2023-02-17 21:43:17 +0200

    fixed button

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,30 +1,14 @@
 import React, { Suspense } from 'react';
-//import { Outlet } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
-import { Button } from './UI/Button/Button';
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <Suspense fallback={<div>Loading...</div>}>
-        Hello World
+        {/* Hello World */}
         {/* <Outlet /> */}
-        <Button type={'submit'} color={'accent'} design={'home'}>
-          Login
-        </Button>
-        <Button type={'button'} color={'white'} design={'home'}>
-          Registration
-        </Button>
-        <Button type={'button'} color={'accent'} design={'operation'}>
-          Input
-        </Button>
-        <Button type={'button'} color={'white'} design={'operation'}>
-          Clear
-        </Button>
-        <Button type={'button'} color={'white'} design={'modal'}>
-          No
-        </Button>
       </Suspense>
     </div>
   );

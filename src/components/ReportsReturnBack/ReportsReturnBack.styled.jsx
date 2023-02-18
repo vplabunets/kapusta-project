d1256f621a56a9fbf6c4389f6fe4393d88159558
Darymee <danieledaniele3005@gmail.com>
2023-02-19 00:47:37 +0100

    add reports return link

---
+++ src/components/ReportsReturnBack/ReportsReturnBack.styled.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+import device from 'constants/deviceSize';
+
+export const BackLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+
+  & > p {
+    display: none;
+
+    @media ${device.tablet} {
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 1.17;
+
+      letter-spacing: 0.04em;
+
+      color: rgba(82, 85, 95, 0.7);
+      display: block;
+      margin-left: 15px;
+    }
+    @media ${device.laptop} {
+      margin-left: 16px;
+    }
+  }
+`;
--- src/pages/ReportsPage/ReportsPage.jsx
+++ src/pages/ReportsPage/ReportsPage.jsx
@@ -2,10 +2,12 @@ import ReportsBalanceContainer from 'components/ReportsBalanceContainer/ReportsB
 import React from 'react';
 import ReportsPageWrapper from './ReportsPage.styled';
 import ReportsMonthBalance from 'components/ReportsMonthBalance/ReportsMonthBalance';
+import { ReportsReturnBack } from 'components/ReportsReturnBack/ReportsReturnBack';
 
 const ReportsPage = () => {
   return (
     <ReportsPageWrapper>
+      <ReportsReturnBack />
       <ReportsMonthBalance />
       <ReportsBalanceContainer />
     </ReportsPageWrapper>

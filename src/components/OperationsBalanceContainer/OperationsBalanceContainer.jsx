52122e6a382c6d83a91bef336df6c4bf84cd0267
Yuliia-Bakalova <98843051+Yuliia-Bakalova@users.noreply.github.com>
2023-02-18 17:54:14 +0200

    add reports, balance modal and fix balance

---
+++ src/components/OperationsBalanceContainer/OperationsBalanceContainer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Chart from 'images/icons-sprite.svg';
+import { NavLink } from 'react-router-dom';
+
+import {  Title, Wrap} from './OperationsBalanceContainer.styled';
+
+
+export default function Reports() {
+  return (
+      <Wrap>
+       <NavLink
+            to="/reports"
+            style={isActive => ({
+                color: 'rgba(82, 85, 95, 0.7)',
+                display: 'flex',
+                fontStyle: 'normal',
+                fontWeight: 'normal',
+                fontSize: '12px',
+                lineHeight: '14px',
+                letterSpacing: '0.04em',
+                alignItems: 'center',
+                textAlign: 'center',
+                 textDecoration: 'none',
+            })}
+        >
+            <Title>Reports</Title>
+           <svg alt="logo" width={24} height={24}>
+           <use href={`${Chart}#icon-chart`}></use>
+        </svg> 
+      </NavLink>
+      </Wrap>
+  );
+}
--- src/components/OperationsBalanceModal/OperationsBalanceModal.jsx
+++ src/components/OperationsBalanceModal/OperationsBalanceModal.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import {
+  Backdrop,
+  Text,
+  Title,
+  Modal
+} from './OperationsBalanceModalstyled';
+
 
 const OperationsBalanceModal = () => {
-  return <div></div>;
-};
+  return (
+      <Backdrop >
+    <Modal>
+      <Title>
+        Hello! To get started, enter the current balance of your account!
+      </Title>
+      <Text>
+        You can't spend money until you have it
+      </Text>
+    </Modal>
+  </Backdrop>
+  )
 
+}
 export default OperationsBalanceModal;

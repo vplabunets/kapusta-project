811b964595050e3831b8e7eb831509d9e82d27b0
Volodymyr Labunets <vplabunets@gmail.com>
2023-02-18 23:40:54 +0200

    fixed Balance container
    added OperationsForwardToReports

--- src/components/OperationsBalanceContainer/OperationsBalanceContainer.jsx
+++ src/components/OperationsBalanceContainer/OperationsBalanceContainer.jsx
@@ -1,34 +1,14 @@
-
+import Balance from 'components/Balance/Balance';
+import OperationsForwardToReports from 'components/OperationsForwardToReports/OperationsForwardToReports';
 import React from 'react';
-import Chart from 'images/icons-sprite.svg';
-import { NavLink } from 'react-router-dom';
 
-import {  Title, Wrap} from './OperationsBalanceContainer.styled';
-
-
-export default function Reports() {
+const OperationsBalanceContainer = () => {
   return (
-      <Wrap>
-       <NavLink
-            to="/reports"
-            style={isActive => ({
-                color: 'rgba(82, 85, 95, 0.7)',
-                display: 'flex',
-                fontStyle: 'normal',
-                fontWeight: 'normal',
-                fontSize: '12px',
-                lineHeight: '14px',
-                letterSpacing: '0.04em',
-                alignItems: 'center',
-                textAlign: 'center',
-                 textDecoration: 'none',
-            })}
-        >
-            <Title>Reports</Title>
-           <svg alt="logo" width={24} height={24}>
-           <use href={`${Chart}#icon-chart`}></use>
-        </svg> 
-      </NavLink>
-      </Wrap>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <Balance />
+      <OperationsForwardToReports />
+    </div>
   );
-}
+};
+
+export default OperationsBalanceContainer;

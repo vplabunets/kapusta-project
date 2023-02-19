b679618ca4fb99185f934983ec86bc726fcfae45
VolodymyrTashak <vovavozny33@gmail.com>
2023-02-19 10:01:30 +0200

    add Loader

---
+++ src/components/Loader/Loader.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const LoaderBox = styled.div`
+position: absolute;
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+z-index: 1;
+`;

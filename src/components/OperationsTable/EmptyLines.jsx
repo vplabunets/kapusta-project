9661d88d91e267609afa319b2f44550f04dcfbec
VolodymyrTashak <vovavozny33@gmail.com>
2023-02-19 09:34:45 +0200

    add styles OperationsTable

---
+++ src/components/OperationsTable/EmptyLines.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Empty } from './EmptyLines.styled';
+
+const EmptyLines = () => {
+    return (
+      <>
+        <Empty>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </Empty>
+        <Empty>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </Empty>
+        <Empty>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </Empty>
+      </>
+    );
+  };
+  
+  export default EmptyLines;

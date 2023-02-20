635416d0ebd727f7a8c1841a3f3fe52f5f977181
Yuliia-Bakalova <98843051+Yuliia-Bakalova@users.noreply.github.com>
2023-02-20 16:02:36 +0200

    current period

---
+++ src/components/ReportsCurrentPeriod/ReportsCurrentPeriod.styled.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+// import device from 'constants/deviceSize';
+
+export const Wrapper = styled.div`
+
+`;
+export const Title = styled.h2`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.143;
+  color: rgba(82, 85, 95, 0.7);
+  text-align:center;
+
+`;
+export const ContainerPeriod = styled.div`
+display: flex;
+justify-content: center;
+margin-top: 7px;
+`;
+
+export const Text = styled.text`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 1.143;
+    letter-spacing: 0.02em;
+    text-transform: uppercase;
+    color: var(--black-text-color);
+    margin-right: 7px;
+    margin-left: 7px;
+`;
+
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.02em;
+  text-transform: uppercase;
+  border:white;
+  cursor: pointer;
+  background-color: transparent;
+`
+
+

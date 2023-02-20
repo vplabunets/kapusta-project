f7ae6a095ba8573017039d0e476d318268e2b3d1
Darymee <danieledaniele3005@gmail.com>
2023-02-20 04:28:51 +0100

    add background animation

---
+++ src/components/UI/AnimateBackground/AnimateBackground.jsx
@@ -0,0 +1,92 @@
+import Icon from 'images/icons-sprite.svg';
+
+import { BackgroundGrey, Item } from './AnimateBackground.styled';
+
+export const AnimateBackground = () => {
+  return (
+    <BackgroundGrey>
+      <ul>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+        <Item>
+          <svg width="83" height="85">
+            <use href={`${Icon}#icon-cabbage-left`}></use>
+          </svg>
+        </Item>
+      </ul>
+    </BackgroundGrey>
+  );
+};
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -2,58 +2,6 @@ import styled from 'styled-components';
 
 import device from 'constants/deviceSize';
 
-export const BackgroundGrey = styled.div`
-  z-index: -1;
-  width: 100%;
-  height: 342px;
-
-  position: absolute;
-  top: 0;
-  background-color: #f5f6fb;
-  border-bottom-left-radius: 20%;
-
-  overflow: hidden;
-
-  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
-    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
-
-  @media ${device.tablet} {
-    height: 582px;
-  }
-  @media ${device.laptop} {
-    height: 582.5px;
-  }
-`;
-
-export const DesignWrapper = styled.div`
-  overflow: hidden;
-  position: absolute;
-  top: 160px;
-  right: 0;
-  z-index: -1;
-  & > svg {
-    width: 83px;
-    height: 83px;
-
-    transform: rotate(-180deg);
-
-    @media ${device.tablet} {
-      display: none;
-    }
-  }
-  & > img {
-    display: none;
-    @media ${device.tablet} {
-      display: block;
-      top: 85px;
-    }
-    @media ${device.laptop} {
-      display: block;
-      width: 100vw;
-    }
-  }
-`;
-
 export const HomePageWrapper = styled.div`
   padding: 0 20px;
   margin: 0 auto;

7d9c3b1508f510cfa64e46e200b091b92ff5ed6c
DenisKhrebtov <khrebtovdenis@gmail.com>
2023-02-21 01:57:10 +0200

    add modalLogic

--- src/components/ConfirmModal/ConfirmModal.jsx
+++ src/components/ConfirmModal/ConfirmModal.jsx
@@ -10,44 +10,73 @@ import {
   Text,
   ButtonWrapper,
 } from "./ConfirmModal.styled";
+import { useEffect } from 'react';
 
+const body = document.querySelector("body");
+ 
+const ConfirmModal = ({ text, setModalOpen }) => {
+    
+    const closeModal = () => {
+    setModalOpen(false);
+  };
 
-const ConfirmModal = ({text}) => {
+    const handleEscapeClose = e => {
+    if (e.code === "Escape") {
+      closeModal();
+    }
+      }
+    
+    const handleBackDropClose = e => {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+      }
+    
+    useEffect(() => {
+      window.addEventListener("keydown", handleEscapeClose);
+      return () => {
+        window.removeEventListener("keydown", handleEscapeClose);
+        body.classList.toggle("no-scroll");
+      }
+    });
 
-  return createPortal(
-    <Backdrop>
-      <Modal>
-        <ButtonClose>
-          <svg>
-            <use href={`${icon}#icon-close`}></use>
-          </svg>
-        </ButtonClose>
-        <div>
-          <Text>{text}</Text>
-          <ButtonWrapper>
-            <Button
-              type={"button"}
-              color={"accent"}
-              design={"modal"}
-            >
-              YES
-            </Button>
-            <Button
-              type={"button"}
-              color={"white"}
-              design={"modal"}>
-              NO
-            </Button>
-          </ButtonWrapper>
-        </div>
-      </Modal>
-    </Backdrop>,
-    document.querySelector('#modal-root')
-  );
-};
+    return createPortal(
+      <Backdrop onClick={handleBackDropClose}>
+        <Modal>
+          <ButtonClose onClick={closeModal}>
+            <svg>
+              <use href={`${icon}#icon-close`}></use>
+            </svg>
+          </ButtonClose>
+          <div>
+            <Text>{text}</Text>
+            <ButtonWrapper>
+              <Button
+                closeModal={closeModal}
+                type={"button"}
+                color={"accent"}
+                design={"modal"}
+              >
+                YES
+              </Button>
+              <Button
+                onClick={closeModal}
+                type={"button"}
+                color={"white"}
+                design={"modal"}>
+                NO
+              </Button>
+            </ButtonWrapper>
+          </div>
+        </Modal>
+      </Backdrop>,
+      document.querySelector('#modal-root')
+    );
+  };
+
+  ConfirmModal.propTypes = {
+  text: PropTypes.string.isRequired,
+}
 
 export default ConfirmModal;
 
-ConfirmModal.propTypes = {
-  text: PropTypes.string.isRequired,
-}
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Avatar } from '@mui/material';
+import ConfirmModal from 'components/ConfirmModal/ConfirmModal';
 
 import ExitIcon from 'images/icons-sprite.svg';
 
@@ -10,32 +12,40 @@ import {
   LogoutBtn,
 } from './NavBar.styles';
 
+
+
 const NavBar = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  
+
   return (
-    <Container>
-      <Avatar
-        sx={{
-          width: 32,
-          height: 32,
-          color: 'rgba(82, 85, 95, 1)',
-          backgroundColor: 'rgba(245, 246, 250, 1)',
-          fontWeight: 700,
-          fontSize: '12px',
-          lineHeight: 1.17,
-        }}
-      >
-        U
-      </Avatar>
-      <ExitContainer>
-        <UserName>User Name</UserName>
-        <ExitBtn>Exit</ExitBtn>
-      </ExitContainer>
-      <LogoutBtn>
-        <svg alt="exit" width={16} height={16}>
-          <use href={`${ExitIcon}#icon-logout`}></use>
-        </svg>
-      </LogoutBtn>
-    </Container>
+    <>
+      <Container>
+        <Avatar
+          sx={{
+            width: 32,
+            height: 32,
+            color: 'rgba(82, 85, 95, 1)',
+            backgroundColor: 'rgba(245, 246, 250, 1)',
+            fontWeight: 700,
+            fontSize: '12px',
+            lineHeight: 1.17,
+          }}
+        >
+          U
+        </Avatar>
+        <ExitContainer>
+          <UserName>User Name</UserName>
+          <ExitBtn onClick={() => setModalOpen(true)}>Exit</ExitBtn>
+        </ExitContainer>
+        <LogoutBtn onClick={() => setModalOpen(true)}>
+          <svg alt="exit" width={16} height={16}>
+            <use href={`${ExitIcon}#icon-logout`}></use>
+          </svg>
+        </LogoutBtn>
+      </Container>
+      {modalOpen && <ConfirmModal setModalOpen={setModalOpen} text={'Do you really want to leave?' } /> }  
+      </>
   );
 };
 

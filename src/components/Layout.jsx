c9b4d9819446cff6346cb20018725b9d637913f7
Volodymyr Labunets <vplabunets@gmail.com>
2023-02-22 13:44:10 +0200

    add lazy loader

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,14 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
+import LoaderCabbage from './LoaderCabbage/LoaderCabbage';
 import ToastifyContainer from './UI/ToastifyContainer/ToastifyContainer';
 
 const Layout = () => {
   return (
     <div>
       <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        {/* Hello World */}
+      <Suspense fallback={<LoaderCabbage />}>
         <ToastifyContainer />
         <Outlet />
       </Suspense>

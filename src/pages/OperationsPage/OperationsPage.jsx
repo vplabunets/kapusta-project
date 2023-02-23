1d1463c0cef0c2ac60dd90c1718fff1b25aa3e0b
linluxx <karolina.kkv@gmail.com>
2023-02-23 23:04:55 +0200

    fixed operations switching

--- src/pages/OperationsPage/OperationsPage.jsx
+++ src/pages/OperationsPage/OperationsPage.jsx
@@ -1,4 +1,4 @@
-import operationTypes from 'constants/operationTypes';
+// import operationTypes from 'constants/operationTypes';
 
 import OperationsBalanceContainer from 'components/OperationsBalanceContainer/OperationsBalanceContainer';
 import OperationsBalanceModal from 'components/OperationsBalanceModal/OperationsBalanceModal';
@@ -7,26 +7,30 @@ import OperationsTypeSwitcher from 'components/OperationsTypeSwitcher/Operations
 
 import { Background } from 'components/UI/Background/Background';
 import OperationsPageWrapper from './OperationsPages.styled';
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   getSummary,
   getTransactionsByOperation,
 } from 'redux/transaction/operations';
-import { setOperationType } from 'redux/transaction/slice';
+// import { setOperationType } from 'redux/transaction/slice';
+import { selectOperationType } from 'redux/transaction/selectors';
 
 const OperationsPage = () => {
-  const [type, setType] = useState(operationTypes.expenses);
+  // const [type, setType] = useState(operationTypes.expenses);
+
   // const [balance, setBalance] = useState(null);
   // const addBalance = xxx => setBalance(xxx);
+
+  const operationType = useSelector(selectOperationType);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(setOperationType(type));
-    dispatch(getSummary({ operation: type }));
-    dispatch(getTransactionsByOperation({ operation: type }));
-  }, [dispatch, type]);
+    // dispatch(setOperationType(type));
+    dispatch(getSummary({ operation: operationType }));
+    dispatch(getTransactionsByOperation({ operation: operationType }));
+  }, [dispatch, operationType]);
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -35,7 +39,9 @@ const OperationsPage = () => {
       <OperationsPageWrapper>
         <OperationsBalanceContainer addBalance={0} />
         {false && <OperationsBalanceModal />}
-        <OperationsTypeSwitcher type={type} setType={setType} />
+        <OperationsTypeSwitcher
+        // type={type} setType={setType}
+        />
         <OperationsContainer />
       </OperationsPageWrapper>
     </div>

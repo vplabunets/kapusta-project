727605b10c4e79024b768e7b0b2e8f2b7a025c4d
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2023-02-24 16:38:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -54,7 +54,7 @@ export const App = () => {
                 <PrivateRoutes redirectTo="/" component={<ReportsPage />} />
               }
             />
-            <Route path="*" element={<Navigate to="/" />} />
+          //  <Route path="*" element={<Navigate to="/" />} />
           </Route>
         </Routes>
       )}

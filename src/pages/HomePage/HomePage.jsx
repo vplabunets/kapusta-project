2fd3c67130f582a0d43b5ada4de1e1fb2e4accc2
Oleksii <achili3f17@gmail.com>
2023-02-26 19:38:24 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,7 +9,7 @@ import {
   CabbageContainer,
 } from './HomePage.styled';
 import { useEffect } from 'react';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setToken } from 'redux/auth/slice';
 import { refreshUser } from 'redux/auth/operations';
@@ -18,35 +18,20 @@ import { selectAccessToken } from 'redux/auth/selectors';
 const HomePage = () => {
   const dispatch = useDispatch();
   const [searchParams] = useSearchParams();
-
   const reduxAccessToken = useSelector(selectAccessToken);
-  // const search = useLocation().search;
 
   useEffect(() => {
     const accessToken = searchParams.get('accessToken');
-    // const refreshToken = searchParams.get('refreshToken');
-    // const sid = searchParams.get('sid');
-
     if (!accessToken) {
       return;
     }
 
     dispatch(setToken(accessToken));
     dispatch(refreshUser());
-    // dispatch(googleAuth({ accessToken, refreshToken, sid }));
-    // dispatch(authOperations.getUserData());
   }, [dispatch, searchParams]);
 
   if (reduxAccessToken === '' || reduxAccessToken === null) {
-    // const accessToken = new URLSearchParams(search).get('accessToken');
-    // console.log(accessToken);
-    // dispatch(setToken(accessToken));
   }
-  // dispatch(refreshUser());
-
-  // useEffect(() => {
-  //   dispatch(refreshUser());
-  // }, [dispatch]);
 
   return (
     <>

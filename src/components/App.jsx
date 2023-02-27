663fe39cb51e657ac057bfdef0d494518be6ccd1
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2023-02-27 15:45:27 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -46,7 +46,7 @@ export const App = () => {
             <Route path="/google-redirect" element={
                 <PrivateRoutes 
                   redirectTo="/"
-                  component={<HomePage />}
+                  component={<OperationsPage />}
                 />
               } />
             <Route
